68fd55e2577a752e8440986456d2261a4ba732e1
ORX AI <131006215+orxai@users.noreply.github.com>
2024-09-23 22:19:14 +0330

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+GITHUB_PUSH_SECRET=$1
+DOCKER_IMAGE_NAME=$2
+DOCKER_IMAGE_TAG=$3
+DOCKERFILE_PATH=$4
+BUILD_CONTEXT=$5
+
+# GITHUB_REPOSITORY is always org/repo syntax. Get the owner in case it is different than the actor (when working in an org)
+GITHUB_OWNER=`echo ${GITHUB_REPOSITORY} | cut -d/ -f1`
+
+# Set up full image with tag
+IMAGE_ID=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
+IMAGE_ID=$(echo ${IMAGE_ID} | tr '[A-Z]' '[a-z]')
+
+# Login to ghcr
+echo ${GITHUB_PUSH_SECRET} | docker login https://ghcr.io -u ${GITHUB_ACTOR} --password-stdin
+
+# Build image
+docker build -t ${IMAGE_ID} -f ${DOCKERFILE_PATH} ${BUILD_CONTEXT}
+
+# Push image
+docker push ${IMAGE_ID}
