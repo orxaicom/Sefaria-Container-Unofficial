14e8f18ccd7a3906392626ba4ff113d6c19821d9
ORX AI <131006215+orxai@users.noreply.github.com>
2024-09-24 18:08:40 +0330

    Update install_dependencies.sh [skip ci]

--- deps/install_dependencies.sh
+++ deps/install_dependencies.sh
@@ -69,9 +69,6 @@ cd .. && rm -rf Sefaria-Project
 # https://github.com/moby/moby/issues/45757
 chown -R root:root /usr/local/lib/node_modules
 
-# This is for DevContainers to use as workspace
-mkdir /app
-
 # Clean the unwanted packages
 apt-get -y remove --purge unzip git gnupg curl
 apt-get -y autoremove
